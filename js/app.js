2362ce48ba6007dc2659072afc6723458efaaa00
ribbit76 <bmachock@live.com>
2015-12-14 11:36:45 -0700

    Final Commit
    working page

--- js/app.js
+++ js/app.js
@@ -31,7 +31,17 @@ $(document).ready(function() {
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
+	})
+
+	$(document).keydown(function(event) {
+		if(event.which === 88) {
+          $('.ryu-still').hide();
+          $('.ryu-ready').hide();
+          $('ryu-throwing').hide();
+          $('.ryu-cool').show();
+		}
 	});
+
 
 });
 
@@ -39,5 +49,6 @@ function playHadouken () {
 	$('#hadouken-sound')[0].volume= 0.5;
 	$('#hadouken-sound')[0].load();
 	$('#hadouken-sound')[0].play();
+
 
 }
