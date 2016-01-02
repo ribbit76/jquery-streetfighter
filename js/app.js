1d1396745ea8b99a8a0231eb97d4490aae06dce7
ribbit76 <bmachock@live.com>
2016-01-02 12:04:39 -0700

    Working SF Project
    complete

--- js/app.js
+++ js/app.js
@@ -1,54 +1,65 @@
-$(document).ready(function() {
-	$('.ryu').mouseenter(function() {
-		$('.ryu-still').hide();
-		$('.ryu-ready').show();
-	})
-	.mouseleave(function() {
-		$('.ryu-ready').hide();
-		$('.ryu-still').show();
-	})
-	.mousedown(function() {
-		playHadouken();
-		$('.ryu-ready').hide();
-		$('.ryu-throwing').show();
-		$('.hadouken').show().animate( 
-			{'left': '1020px'},
-			500,
-			function() {
-				$('this').hide();
-				$('this').css('left', '520px');
-			});
-		$('.hadouken').finish().show()
-			.animate(
-			{'left': '1020px'},
-			500,
-			function() {
-				$(this).hide();
-				$(this).css('left', '520px');
-			}
-			);
-	})
-	.mouseup(function() {
-		$('.ryu-throwing').hide();
-		$('.ryu-ready').show();
-	})
 
-	$(document).keydown(function(event) {
-		if(event.which === 88) {
-          $('.ryu-still').hide();
-          $('.ryu-ready').hide();
-          $('ryu-throwing').hide();
-          $('.ryu-cool').show();
-		}
-	});
+$(document).ready(function(){
+    $('.ryu').mouseenter(function(){
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function() {
+        $('.ryu-still').show();
+        $('.ryu-ready').hide();
+    })
+    .mousedown(function(){
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-still').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate(
+            {'left': '1020px'},
+            500,
+            function(){
+                $(this).hide();
+                $(this).css('left', '520px');
+            });
+    })
+    .mouseup(function(){
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    });
+  $(document).keydown(function(event) {
+    if(event.which === 88) {
+        $('.ryu-ready').hide();
+        $('.ryu-still').hide();
+        $('.ryu-cool').show();
+        playCool();
+    }
+  });
 
+  $(document).keyup(function(event) {
+    if(event.which === 88) {
+        $('.ryu-cool').hide();
+        $('.ryu-still').show();
+      //stop playcool() function
+          playCoolStop();
+    }
+  });
 
 });
 
 function playHadouken () {
-	$('#hadouken-sound')[0].volume= 0.5;
-	$('#hadouken-sound')[0].load();
-	$('#hadouken-sound')[0].play();
-
+  $('#hadouken-sound')[0].volume= 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
 
 }
+
+function playCool () {
+  $('#cool-sound')[0].volume= 0.5;
+  //$('#cool-sound')[0].load();
+  $('#cool-sound')[0].play();
+
+}
+function playCoolStop () {
+  $('#cool-sound')[0].pause();
+  $('#cool-sound')[0].load();
+}
+
